e81f227d7db9ceb42c20d9849f288d569375df73
BrainBytes <info@brainbyt.es>
2026-03-11 21:29:43 +0100

    refactor: move shadcn-ui to packages for consistency
    - Create packages/shadcn-ui with all shadcn/ui components
    - Remove components from apps/web, import from @repo/shadcn-ui
    - Symmetric structure: packages/shadcn-ui & packages/nativewindui
    - Easier to version, share, and extend UI packages

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,2 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+// Re-export utils from the package
+export { cn } from "@repo/shadcn-ui/lib/utils";
